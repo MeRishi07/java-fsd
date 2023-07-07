8aeca5422bd5e78a96a94fa93113056eae83030d
Saptarshi <136781153+MeRishi07@users.noreply.github.com>
2023-07-07 16:26:27 +0530

    Add files via upload

---
+++ Phase2/Assisted_Project1/SessionTracking_URL/src/main/java/com/interfac/DashboardServlet.java
@@ -0,0 +1,24 @@
+package com.interfac;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class DashboardServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("username") != null) {
+            String username = session.getAttribute("username").toString();
+            response.getWriter().println("Welcome to the dashboard, " + username);
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+    }
+}
