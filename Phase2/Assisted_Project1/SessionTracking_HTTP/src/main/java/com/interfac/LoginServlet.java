7f58b091084fa897295d87fc852e9b681355a471
Saptarshi <136781153+MeRishi07@users.noreply.github.com>
2023-07-07 16:43:25 +0530

    Add files via upload

---
+++ Phase2/Assisted_Project1/SessionTracking_HTTP/src/main/java/com/interfac/LoginServlet.java
@@ -0,0 +1,39 @@
+package com.interfac;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Validate the login credentials (e.g., against a database)
+        boolean isValidLogin = validateLogin(username, password);
+
+        if (isValidLogin) {
+            // Create a new session or retrieve an existing one
+            HttpSession session = request.getSession(true);
+            session.setAttribute("username", username);
+
+            response.sendRedirect("dashboard.jsp");
+        } else {
+            response.sendRedirect("login.jsp?error=1");
+        }
+    }
+
+    private boolean validateLogin(String username, String password) {
+        // Implement your login validation logic here
+        // Return true if the login is valid, false otherwise
+        // This is just a placeholder method
+        return username.equals("admin") && password.equals("password");
+    }
+}
---
+++ Phase2/Assisted_Project1/SessionTracking_HiddenfromFiles/src/main/webapp/login.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <% String error = request.getParameter("error"); %>
+    <% if (error != null && error.equals("1")) { %>
+        <p style="color: red;">Invalid username or password. Please try again.</p>
+    <% } %>
+    <form action="login" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="hidden" name="sessionID" value="${pageContext.session.id}">
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
