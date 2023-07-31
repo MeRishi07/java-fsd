e6d90561665b84581078ec91e6ac217290da9f79
Saptarshi <136781153+MeRishi07@users.noreply.github.com>
2023-07-31 12:24:09 +0530

    Add files via upload

---
+++ Phase3/PracticeProject1/EventExample/src/main/java/com/event/Test.java
@@ -0,0 +1,22 @@
+package com.event;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("application.xml");
+		
+		//Let us raise a start event
+		context.start();
+		HelloWorld obj=(HelloWorld) context.getBean("helloworld");
+		System.out.println(obj.getMessage());
+		//Let us raise a stop event
+		context.stop();
+		
+	}
+
+}
